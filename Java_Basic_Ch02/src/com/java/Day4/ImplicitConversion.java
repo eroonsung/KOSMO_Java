0e8e2b2573129fe7907187af617a5bdbc98e97dd
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-17 18:08:43 +0900

    Update ImplicitConversion.java

--- Java_Basic_Ch02/src/com/java/Day4/ImplicitConversion.java
+++ Java_Basic_Ch02/src/com/java/Day4/ImplicitConversion.java
@@ -4,7 +4,7 @@ public class ImplicitConversion {
 
 	public static void main(String[] args) {
 		byte bNum = 10;
-		int iNum = bNum; // byte형 값이 int형 변수로 대입됨
+		int iNum = bNum; 
 		
 		System.out.println(bNum); //10
 		System.out.println(iNum); //10
