57b3d276209da9f543e652993ba7edad797afa3e
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-17 17:50:23 +0900

    20210517

---
+++ Java_Basic_Ch02/src/com/java/Day4/CharacterEx2.java
@@ -0,0 +1,12 @@
+package com.java.Day4;
+
+public class CharacterEx2 {
+
+	public static void main(String[] args) {
+		char ch1 = 'วั';
+		char ch2 = '\uD55C';
+		
+		System.out.println(ch1); //วั
+		System.out.println(ch2); //วั
+	}
+}
