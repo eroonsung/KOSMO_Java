7a83b3c58154c4b4b8c4edcae5b7e857c91a547c
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-20 18:35:52 +0900

    20210520

--- Java_Basic_Ch07/src/com/java/Day6/ArrayPractice9.java
+++ Java_Basic_Ch07/src/com/java/Day6/ArrayPractice9.java
@@ -3,8 +3,8 @@ package com.java.Day6;
 public class ArrayPractice9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] strArray = new String[6];
+		
 	}
 
 }
