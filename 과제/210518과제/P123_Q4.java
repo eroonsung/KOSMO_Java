94f898365f21fafadf4c992ab75984e6a5a677fc
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-20 09:55:26 +0900

    Update P123_Q4.java

--- 과제/210518과제/P123_Q4.java
+++ 과제/210518과제/P123_Q4.java
@@ -2,24 +2,6 @@ package HW_210518;
 
 public class P123_Q4 {
 	public static void main(String[] args) {
-//		int line = 4;  
-//		int star = 1;
-//		int space = line/2 +1;
-//		
-//		for(int i = 0; i<line; i++) {
-//			for(int j = 0; j<space; j++) {
-//				System.out.print(' ');
-//			}
-//			for(int j=0; j<star; j++) {
-//				System.out.print('*');
-//			}
-//			for(int j = 0; j<space; j++) {
-//				System.out.print(' ');
-//			}
-//			space-=1;
-//			star+=2;
-//			System.out.println();
-//		}
 		
 		int line;
 		int space = 4;
