d8c554ed918ac0a104bbb71cca2f526b3c8a5670
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-24 10:21:10 +0900

    20210524

--- 과제/210521과제/HW5.java
+++ 과제/210521과제/HW5.java
@@ -18,8 +18,8 @@ public class HW5 {
 			for(int j = 0; j < array.length-i-1; j++) {
 				if(array[j]<array[j+1]) {
 					int temp = array[j];
-	                array[j] = array[j+1];
-	                array[j+1] = temp;
+	                		array[j] = array[j+1];
+	                		array[j+1] = temp;
 				}
 			}
 		}
