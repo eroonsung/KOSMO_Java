2f46dc9700d666728d66cfceeb499a4f8cde3ad3
Eroon Sung <77269204+eroonsung@users.noreply.github.com>
2021-05-24 10:03:03 +0900

    20210524

--- 과제/210521과제/HW1.java
+++ 과제/210521과제/HW1.java
@@ -7,10 +7,15 @@ public class HW1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("3~10 사이의 자연수를 입력하세요.");
 		int num = scan.nextInt();
 		char out = 'A';
 		
 		for(int i = 0; i <num; i++) {
+			if((n<3)||(n>10)){
+				System.out.println("범위를 벗어났습니다.")
+				break;
+			}
 			for(int j=0; j<num-i; j++, out++) {
 				System.out.print(out);
 			}
